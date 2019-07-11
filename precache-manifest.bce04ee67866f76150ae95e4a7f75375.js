46c5347aa1c7a9205ea81f776b9ace4054a3b141
Vanchurick <davane4ek@gmail.com>
2019-07-11 18:23:24 +0300

    Updates

--- precache-manifest.bce04ee67866f76150ae95e4a7f75375.js
+++ precache-manifest.bce04ee67866f76150ae95e4a7f75375.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "0653b62339844716c21b5a8ae97f73e3",
+    "revision": "e1eaffcdd6b5bdbdbd3f92762d7c6af1",
     "url": "/goit-react-hw-04-reader/index.html"
   },
   {
-    "revision": "38d9690194d84228a4ae",
+    "revision": "9f118ac67f27f1846702",
     "url": "/goit-react-hw-04-reader/static/css/main.829edca0.chunk.css"
   },
   {
-    "revision": "9a2880b4c671e677743e",
-    "url": "/goit-react-hw-04-reader/static/js/2.cc963c7d.chunk.js"
+    "revision": "85278a2648eae4a7cde2",
+    "url": "/goit-react-hw-04-reader/static/js/2.53639317.chunk.js"
   },
   {
-    "revision": "38d9690194d84228a4ae",
-    "url": "/goit-react-hw-04-reader/static/js/main.6c9c1801.chunk.js"
+    "revision": "9f118ac67f27f1846702",
+    "url": "/goit-react-hw-04-reader/static/js/main.9292d2cc.chunk.js"
   },
   {
     "revision": "84bf89e08b01550b9b7d",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/goit-react-hw-04-reader/precache-manifest.e41155bf5ca452fff977f109df3fe0a8.js"
+  "/goit-react-hw-04-reader/precache-manifest.bce04ee67866f76150ae95e4a7f75375.js"
 );
 
 self.addEventListener('message', (event) => {
