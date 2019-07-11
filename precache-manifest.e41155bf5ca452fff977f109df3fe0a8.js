b703489af2a37ef293d5c6241e907e3e1f356d4e
Vanchurick <davane4ek@gmail.com>
2019-07-11 18:21:27 +0300

    Updates

---
+++ precache-manifest.e41155bf5ca452fff977f109df3fe0a8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0653b62339844716c21b5a8ae97f73e3",
+    "url": "/goit-react-hw-04-reader/index.html"
+  },
+  {
+    "revision": "38d9690194d84228a4ae",
+    "url": "/goit-react-hw-04-reader/static/css/main.829edca0.chunk.css"
+  },
+  {
+    "revision": "9a2880b4c671e677743e",
+    "url": "/goit-react-hw-04-reader/static/js/2.cc963c7d.chunk.js"
+  },
+  {
+    "revision": "38d9690194d84228a4ae",
+    "url": "/goit-react-hw-04-reader/static/js/main.6c9c1801.chunk.js"
+  },
+  {
+    "revision": "84bf89e08b01550b9b7d",
+    "url": "/goit-react-hw-04-reader/static/js/runtime~main.860f1cd1.js"
+  }
+]);
